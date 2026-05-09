f97fc0794cf5cc17435fdb858b5d038793d2a305
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-09 16:08:09 -0300

    Refactor fullscreen handling for mobile support

--- source/Main.hx
+++ source/Main.hx
@@ -2,11 +2,13 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxGame;
+import mobile.Fullscreen;
 
 import openfl.display.FPS;
 import openfl.display.Sprite;
-
-import mobile.FullScreen;
+import openfl.display.StageAlign;
+import openfl.display.StageScaleMode;
+import openfl.events.Event;
 
 class Main extends Sprite
 {
@@ -14,9 +16,25 @@ class Main extends Sprite
 	{
 		super();
 
+		if (stage != null)
+			_init();
+		else
+			addEventListener(Event.ADDED_TO_STAGE, _init);
+	}
+
+	function _init(?e:Event):Void
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
+			removeEventListener(Event.ADDED_TO_STAGE, _init);
+
+		stage.align     = StageAlign.TOP_LEFT;
+		stage.scaleMode = StageScaleMode.NO_SCALE;
+
 		addChild(new FlxGame(0, 0, TitleState));
 
-		FullScreen.apply();
+		#if mobile
+		Fullscreen.apply();
+		#end
 
 		#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
