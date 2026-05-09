bb7d59fc7f0361a65df76dda7674cfdcded4abce
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-09 15:34:46 -0300

    Clean up FullScreen.apply function
    Removed unused camera initialization and resizing logic.

--- source/mobile/FullScreen.hx
+++ source/mobile/FullScreen.hx
@@ -1,7 +1,6 @@
 package mobile;
 
 import flixel.FlxG;
-import flixel.FlxCamera;
 import flixel.system.scaleModes.RatioScaleMode;
 
 import openfl.Lib;
@@ -15,9 +14,6 @@ class FullScreen
 	{
 		#if mobile
 
-		if (Lib.current.stage == null)
-			return;
-
 		Lib.current.stage.align = StageAlign.TOP_LEFT;
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
 		Lib.current.stage.displayState = StageDisplayState.FULL_SCREEN_INTERACTIVE;
@@ -26,24 +22,6 @@ class FullScreen
 
 		FlxG.autoPause = false;
 		FlxG.fixedTimestep = false;
-
-		var sw:Int = Lib.current.stage.stageWidth;
-		var sh:Int = Lib.current.stage.stageHeight;
-
-		if (FlxG.camera == null)
-			FlxG.camera = new FlxCamera();
-
-		var ratio:Float = sw / sh;
-
-		if (ratio >= 2.0)
-			FlxG.camera.zoom = 0.70;
-		else if (ratio >= 1.8)
-			FlxG.camera.zoom = 0.80;
-		else
-			FlxG.camera.zoom = 0.90;
-
-		FlxG.resizeWindow(sw, sh);
-		FlxG.resizeGame(sw, sh);
 
 		#end
 	}
