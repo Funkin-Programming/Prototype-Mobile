7a362b5497258bb8995e3682405db8cf80188cd5
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-08 21:32:47 -0300

    Atualizar o Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -9,10 +9,15 @@ class Main extends Sprite
 	public function new()
 	{
 		super();
+
 		addChild(new FlxGame(0, 0, TitleState));
 
 		#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
+		#end
+
+		#if android
+		FlxG.android.preventDefaultKeys = [BACK];
 		#end
 	}
 }
