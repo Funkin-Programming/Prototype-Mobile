a8a81a84128fd82ad3186f6da7c05ca3e715d285
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-09 14:02:50 -0300

    Add scaling and display settings for mobile and desktop

--- source/Main.hx
+++ source/Main.hx
@@ -2,15 +2,30 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxGame;
+import flixel.system.scaleModes.RatioScaleMode;
+
 import openfl.display.FPS;
 import openfl.display.Sprite;
+import openfl.display.StageAlign;
+import openfl.display.StageScaleMode;
+import openfl.display.StageDisplayState;
 
 class Main extends Sprite
 {
 	public function new()
 	{
 		super();
+
+		stage.align = StageAlign.TOP_LEFT;
+		stage.scaleMode = StageScaleMode.NO_SCALE;
+
 		addChild(new FlxGame(0, 0, TitleState));
+
+		FlxG.scaleMode = new RatioScaleMode();
+
+		#if mobile
+		stage.displayState = StageDisplayState.FULL_SCREEN_INTERACTIVE;
+		#end
 
 		#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
