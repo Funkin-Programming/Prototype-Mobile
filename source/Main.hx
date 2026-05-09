8036bc2ae7fdb6be6fefd92186b3a17bf16af240
Brenninho <157061485+Brenninho123@users.noreply.github.com>
2026-05-09 15:11:52 -0300

    Refactor Main class for mobile fullscreen support

--- source/Main.hx
+++ source/Main.hx
@@ -2,14 +2,11 @@ package;
 
 import flixel.FlxG;
 import flixel.FlxGame;
-import flixel.system.scaleModes.RatioScaleMode;
 
 import openfl.display.FPS;
 import openfl.display.Sprite;
-import openfl.display.StageAlign;
-import openfl.display.StageScaleMode;
-import openfl.display.StageDisplayState;
-import openfl.events.Event;
+
+import mobile.FullScreen;
 
 class Main extends Sprite
 {
@@ -17,27 +14,12 @@ class Main extends Sprite
 	{
 		super();
 
-		if (stage != null)
-			_init();
-		else
-			addEventListener(Event.ADDED_TO_STAGE, _init);
-	}
-
-	function _init(?e:Event):Void
-	{
-		if (hasEventListener(Event.ADDED_TO_STAGE))
-			removeEventListener(Event.ADDED_TO_STAGE, _init);
-
-		stage.align     = StageAlign.TOP_LEFT;
-		stage.scaleMode = StageScaleMode.NO_SCALE;
-
 		addChild(new FlxGame(0, 0, TitleState));
 
-		FlxG.scaleMode = new RatioScaleMode();
+		FullScreen.apply();
 
-		#if mobile
-		stage.displayState = StageDisplayState.FULL_SCREEN_INTERACTIVE;
-		#end
+		FlxG.autoPause = false;
+		FlxG.fixedTimestep = false;
 
 		#if !mobile
 		addChild(new FPS(10, 3, 0xFFFFFF));
